799bc367d14020e91410401b204235a90a5e2939
E.Z. Hart <hartez@gmail.com>
2021-09-09 17:57:31 -0600

    Windows mostly working via WrapperView

--- src/Core/src/Handlers/View/ViewHandlerOfT.Windows.cs
+++ src/Core/src/Handlers/View/ViewHandlerOfT.Windows.cs
@@ -11,9 +11,6 @@ namespace Microsoft.Maui.Handlers
 		FrameworkElement? INativeViewHandler.NativeView => this.GetWrappedNativeView();
 		FrameworkElement? INativeViewHandler.ContainerView => ContainerView;
 
-		protected new FrameworkElement? WrappedNativeView =>
-			(FrameworkElement?)base.WrappedNativeView;
-
 		public new WrapperView? ContainerView
 		{
 			get => (WrapperView?)base.ContainerView;
